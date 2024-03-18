852aadeb9035ba9158950380543d4a08b275b4f4
joao diego <jdflorencio@gmail.com>
2024-03-18 08:55:31 -0300

    (feat/swagger):swagger - documentando a api com swagger

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,6 @@ import { JogadoresModule } from './jogadores/jogadores.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
 
-console.log(process.env.MONGODB_URI);
-
 @Module({
   imports: [
     ConfigModule.forRoot({
